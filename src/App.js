d507181ea754fc308b708ccaacb798e47e1306c9
Nicole <nicoleho1314@gmail.com>
2023-11-04 11:48:32 +0800

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='timer'>
+        <header>25 + 5 Clock</header>
+        <div className='length-wrap'>
+          <div id="break-label">
+            <p>Break Length</p>
+            <div className="button-wrap">
+              <div id="break-decrement"><FaArrowDown /></div>
+              <div id="break-length">5</div>
+              <div id="break-increment"><FaArrowUp /></div>
+            </div>
+          </div>
+          <div id="session-label">
+            <p>Session Length</p>
+            <div className='button-wrap'>
+              <div id="session-decrement"><FaArrowDown /></div>
+              <div id="session-length">25</div>
+              <div id="session-increment"><FaArrowUp /></div>
+            </div>
+          </div>
+        </div>
+        <div className='timer-wrap'>
+          <div id="timer-label">Session</div>
+          <div id="time-left">25:00</div>
+        </div>
+        <div className='timer-control'>
+          <button id="start_stop">Start/Stop</button>
+          <button id="reset">Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
